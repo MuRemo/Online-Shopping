68ea4ad21247fb7738a505dc40c4747525549f98
Mohammed2725 <53076332+Mohammed2725@users.noreply.github.com>
2019-08-06 15:14:32 +0530

    updating user interfaces icons

--- app/src/main/java/com/example/fazz/MainActivity.java
+++ app/src/main/java/com/example/fazz/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+	//Buttons
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
         loadingBar = new ProgressDialog(this);
