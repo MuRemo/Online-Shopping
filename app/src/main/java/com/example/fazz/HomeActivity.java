0cf95742ed9b6cfe299ff527926f5eaf59f86ee1
MuRemo <munzirramo18@gmail.com>
2019-09-25 11:56:35 +0530

    Update CartActivity

--- app/src/main/java/com/example/fazz/HomeActivity.java
+++ app/src/main/java/com/example/fazz/HomeActivity.java
@@ -68,8 +68,10 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent intern = new Intent(HomeActivity.this, CartActivity.class);
+                startActivity(intern);
+
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -108,12 +110,24 @@ public class HomeActivity extends AppCompatActivity
         FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
 
                 holder.txtProductName.setText(model.getPname());
                 holder.txtProductDescription.setText(model.getDescription());
                 holder.txtProductPrice.setText("Price = " + model.getPrice() + "Rs");
                 Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intent = new Intent(HomeActivity.this, ProductDetailsActivity.class);
+                        intent.putExtra("pid", model.getPid());
+                        startActivity(intent);
+
+
+                    }
+                });
             }
 
             @NonNull
@@ -171,6 +185,9 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_cart) {
+
+            Intent intern = new Intent(HomeActivity.this, CartActivity.class);
+            startActivity(intern);
 
         } else if (id == R.id.nav_orders) {
 
